c4915838296e35c35d7f6b396a2257bbfe87576a
Taiwo Akinosho <tiwiex@yahoo.com>
2023-05-08 11:30:09 +0100

    my blogs commented out

--- www/my_blogs.py
+++ www/my_blogs.py
@@ -4,5 +4,5 @@ def get_context(context):
     context.test_var = "Hello World"
     return context
 
-def get():
-    return frappe.render_template("templates/my_file.html", context=get_context({}))
+#def get():
+ #   return frappe.render_template("templates/my_file.html", context=get_context({}))
