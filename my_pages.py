9aa2c0563b3f422c4520d7c250129e38ca3f803c
Taiwo Akinosho <tiwiex@yahoo.com>
2023-05-14 15:11:47 +0000

    changes to index and pages

--- my_pages.py
+++ my_pages.py
@@ -3,14 +3,17 @@ import frappe
 
 from frappe import render_template
 
-def get_project_portfolio():
-    return frappe.get_all("Project Portfolio", fields=["name", "project_description"])
+def get_project_data():
+    # Fetch the project data from the database using frappe.get_all or any other suitable method
+    project_data = frappe.get_all("Project Portfolio", filters={}, fields=["name", "project_image"])
+
+    return project_data
 
 def my_pages():
-    data = {
-        "project_portfolio": get_project_portfolio(),
+    context = {
+        "project_portfolio": get_project_data(),
         # Add more data from other doctypes here
     }
-    return render_template("index.html", data=data)
+    return render_template("index.html", context)
 
 
