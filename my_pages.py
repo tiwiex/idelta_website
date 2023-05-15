45ad254418fd40502bd7bd8a744c6bdfe48db2de
Taiwo Akinosho <tiwiex@yahoo.com>
2023-05-15 08:21:10 +0000

    Another commit

--- my_pages.py
+++ my_pages.py
@@ -3,15 +3,15 @@ import frappe
 
 from frappe import render_template
 
-def get_project_data():
+def get_project_portfolio():
     # Fetch the project data from the database using frappe.get_all or any other suitable method
-    project_data = frappe.get_all("Project Portfolio", filters={}, fields=["name", "project_image"])
+    project_portfolio = frappe.get_all("Project Portfolio", filters={}, fields=["name", "project_image"])
 
-    return project_data
+    return project_portfolio
 
 def my_pages():
     context = {
-        "project_portfolio": get_project_data(),
+        "project_portfolio": get_project_portfolio(),
         # Add more data from other doctypes here
     }
     return render_template("index.html", context)
